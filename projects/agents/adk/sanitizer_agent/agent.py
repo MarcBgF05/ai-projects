7d40ed001df249f8bf090d029f411e74d796f66e
MarcBgF05 <00028320@uca.edu.sv>
2026-08-13 17:08:16 -0600

    :tada: new agent

---
+++ projects/agents/adk/sanitizer_agent/agent.py
@@ -0,0 +1,11 @@
+from google.adk import Agent
+from google.adk.models.lite_llm import LiteLlm
+
+orchestrator_agent = Agent(
+    name="",
+    description="",
+    model="",
+    isntruction="",
+)
+
+root_agent = orchestrator_agent
