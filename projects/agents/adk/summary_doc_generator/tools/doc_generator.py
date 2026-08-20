789420095ab2d606b74255a70603495e5dd169a2
MarcBgF05 <00028320@uca.edu.sv>
2026-08-20 10:31:00 -0600

    :construction: add a funct

---
+++ projects/agents/adk/summary_doc_generator/tools/doc_generator.py
@@ -0,0 +1,9 @@
+from docx import Document
+from docx.shared import Pt, RGBColor
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from datetime import datetime
+import json
+
+#TODO: create a format.
+def document_generator(data_json: str) -> str:
+    return data_json    
